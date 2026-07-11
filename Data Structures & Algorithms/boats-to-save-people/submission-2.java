cd2057e5e70509602cac7b1a616124172fc8bea2
NithinMahendra <nithinmamidipelli473@gmail.com>
2026-07-11 23:13:33 +0530

    Add: boats-to-save-people - submission-2

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int count=0,left=0,right=people.length-1;
+        Arrays.sort(people);
+        while(left<=right){
+            if(people[left]+people[right]<=limit)
+                left++;
+            count++;
+            right--;
+        }
+        return count;
+    }
+}
