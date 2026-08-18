3e8d6331f621e0b28b92cad7a48fce4e5d08a933
NithinMahendra <nithinmamidipelli473@gmail.com>
2026-08-18 17:02:52 +0530

    Add: asteroid-collision - submission-4

---
+++ Data Structures & Algorithms/asteroid-collision/submission-4.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> s=new Stack<>();
+        for(int i:asteroids){
+            boolean d=false;
+            if(s.isEmpty() || s.peek()<0 || i>0)s.push(i);
+            else{
+                while(!s.isEmpty() && s.peek()>0 && i<0){
+                   // int max= Math.max(s.peek(),-i);
+                    if(s.peek()>-i){
+                        d=true;
+                        break;
+                    }
+                    else if(s.peek()==-i){
+                        d=true;
+                        s.pop(); break;
+                    }
+                    else{
+                        s.pop();
+                    }
+                }
+                if(!d){s.push(i);}
+            }
+        }
+        return s.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
